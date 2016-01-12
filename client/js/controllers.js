0e34b2427c00f45b6a496a861e7f76a416cd6834
Kayra <KayraAlat@gmail.com>
2016-01-12 21:55:22 +0000

    Client - Added controller

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -7,6 +7,51 @@
 
         var vm = this;
 
+        vm.getContributors = function(){
+            GitService.getContributors()
+            .success(function(response){
+                vm.contributors = response;
+            });
+            .error(function(){
+                vm.error = "The server seems to be down."
+            });
+        };
+
+        vm.getPullRequests = function(){
+            GitService.getPullRequests()
+            .success(function(response){
+                vm.pullRequests = response;
+            });
+            .error(function(){
+                vm.error = "The server seems to be down."
+            });
+        };
+
+        vm.getIssues = function(){
+            GitService.getIssues()
+            .success(function(response){
+                vm.issues = response;
+            });
+            .error(function(){
+                vm.error = "The server seems to be down."
+            });
+        };
+
+        vm.getMostMergesUser = function(){
+            GitService.getMostMergesUser()
+            .success(function(response){
+                vm.user = response;
+            });
+            .error(function(){
+                vm.error = "The server seems to be down."
+            });
+        };
+
+        vm.getContributors();
+        vm.getPullRequests();
+        vm.getIssues();
+        vm.getMostMergesUser();
+
     }]);
 
 });
