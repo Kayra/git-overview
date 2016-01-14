96e6d643696669945b0fcc903f943f592142bafb
Kayra <KayraAlat@gmail.com>
2016-01-14 00:55:22 +0000

    Server - Added models and DB configuration

--- server/git/models.py
+++ server/git/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Contributor(models.Model):
+    name = models.CharField(max_length=255)
+    contributions = models.IntegerField()
+    url = models.CharField(max_length=255)
+    avatar_url = models.CharField(max_length=255)
+    position = models.IntegerField()
+
+
+class PullRequest(models.Model):
+    title = models.CharField(max_length=255)
+    creationDate = models.DateTimeField()
+    url = models.CharField(max_length=255)
+    body = models.TextField()
+    position = models.IntegerField()
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=255)
+    creationDate = models.DateTimeField()
+    url = models.CharField(max_length=255)
+    body = models.TextField()
+    position = models.IntegerField()
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
--- server/tangentsnowball/settings.py
+++ server/tangentsnowball/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'tangentsnowball.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tangentsnowball',
+        'USER': 'tangentsnowball',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
