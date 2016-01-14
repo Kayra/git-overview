6c6f41e1d8a984e32cd85e4a9e14f6c9560c8247
Kayra <KayraAlat@gmail.com>
2016-01-14 00:26:35 +0000

    Server - Installed and configured celery and redis

--- server/tangentsnowball/settings.py
+++ server/tangentsnowball/settings.py
@@ -132,3 +132,11 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Celery
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
